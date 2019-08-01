dfd93ebcc270f5b293ce9735e2042d9edb9e1c06
Takumi Adachi <takumiadachi@gmail.com>
2019-08-01 12:03:26 -0700

    Use pug templating engine.

--- src/server/server.ts
+++ src/server/server.ts
@@ -12,10 +12,13 @@ const app = express();
 app.listen(PORT, () => console.log(`Server started on ${PORT}`));
 
 // express middleware
-app.use("/", express.static(path.join(__dirname, "/public")));
+// Use pug as our templating engine. It is built into express.
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", async (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/index.html"));
+  res.render(path.join(__dirname, "views/login/index.pug"), { title: "hi" });
 });
 
 app.use("/reddit", redditRouter);
