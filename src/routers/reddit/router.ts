9bfbb3f8acbedf3ca20a2f3fc044f1b76d28cb27
Takumi Adachi <takumiadachi@gmail.com>
2019-08-06 13:48:47 -0700

    Added simple auth/!auth after getting access token.

--- src/routers/reddit/router.ts
+++ src/routers/reddit/router.ts
@@ -12,9 +12,15 @@ const REDIRECT_URL = "http://localhost:4201/reddit";
 
 // http://[address]/reddit
 redditRouter.get("/", (req, res) => {
-  res.render(path.join(__dirname, "views/index.pug"), {
-    test: "test"
-  });
+  if (req.session.authenticated) {
+    res.render(path.join(__dirname, "views/index.pug"), {
+      test: "authed"
+    });
+  } else {
+    res.render(path.join(__dirname, "views/index.pug"), {
+      test: "not authed"
+    });
+  }
 });
 
 redditRouter.get("/success", async (req, res) => {
@@ -29,11 +35,25 @@ redditRouter.get("/success", async (req, res) => {
   try {
     const accessToken = await retrieveAccessToken(code);
     console.log(accessToken);
-
+    req.session.authenticated = true;
     res.redirect(REDIRECT_URL);
   } catch (error) {
     console.log(error);
   }
 });
+
+// app.get("/test", async (req, res) => {
+//   console.log(req.session);
+//   if (req.session.views) {
+//     req.session.views++;
+//     res.setHeader("Content-Type", "text/html");
+//     res.write("<p>views: " + req.session.views + "</p>");
+//     res.write("<p>expires in: " + req.session.cookie.maxAge / 1000 + "s</p>");
+//     res.end();
+//   } else {
+//     req.session.views = 1;
+//     res.end("welcome to the session demo. refresh!");
+//   }
+// });
 
 export default redditRouter;
