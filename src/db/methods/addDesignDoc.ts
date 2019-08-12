7b2952acd43c09a7d21e45c73d82f9b961f5542c
Takumi Adachi <takumiadachi@gmail.com>
2019-08-12 15:39:03 -0700

    Using @ts-ignore until i can find a solution for emit not showing up inside the map function.

--- src/db/methods/addDesignDoc.ts
+++ src/db/methods/addDesignDoc.ts
@@ -1,7 +1,6 @@
 import { nano } from "../couchdb/index";
 import * as Nano from "nano";
-import { EventEmitter } from "events";
-import { emit } from "cluster";
+import { TrimmedComment } from "../../models/TrimmedComment";
 
 export async function addDesignDoc(dbName: string) {
   try {
@@ -17,52 +16,41 @@ export async function addDesignDoc(dbName: string) {
   }
 }
 
-class Residence implements Nano.Document {
+class PostView implements Nano.ViewDocument<TrimmedComment> {
   _id: string;
-  _rev: string;
-  name: string;
-  city: string;
-
-  constructor(name, city) {
-    this.name = name;
-    this.city = city;
-  }
-
-  processAPIResponse(response: Nano.DocumentInsertResponse) {
-    if (response.ok === true) {
-      this._id = response.id; //
-      this._rev = response.rev;
-    }
-  }
-}
-// "function(doc) { console.log(doc); emit([doc.name, doc.city], doc); }"
-class ResidenceView implements Nano.ViewDocument<Residence> {
-  _id: string;
-  views: any; // FIGURE OUT THIS TYPE
+  views: { [name: string]: Nano.View<TrimmedComment> };
+  // views: any;
   constructor() {
     this.views = {
-      all: {
-        map: doc => {
-          emit(doc.name, doc._id);
+      ups: {
+        map: function(doc) {
+          // if (doc.ups > 0) {
+          // Ignore until this gets fixed by couchdb-nano devs
+          // @ts-ignore
+          emit(doc.body, doc.ups, doc._id);
+          // }
         }
       }
+      // search_body: {
+      //   map: doc => {
+      //     // Check if lowercase uppercase matters.
+      //     if (doc.body.indexOf(doc.value)) {
+      //       // Value is the search term
+      //       emit(doc.ups, doc.body, doc.created, doc.permalink);
+      //     }
+      //   }
+      // }
     };
   }
 }
 
 (async () => {
-  // const createDb = await nano.db.create("design_query");
-  // console.log(createDb);
-  const db = await nano.use("design_query");
-  // await db.insert(new Residence("Jule", "Shenzhen"));
-  // await db.insert(new Residence("Ange", "Fukuoka"));
-  // await db.insert(new Residence("Shelly", "Macao"));
-  const newView = await new ResidenceView();
-  // const insertDesign = await db.insert(newView, "_design/people");
-  const tryView = await db.view("people", "all", {
-    key: "Jule"
-  });
-  console.log(tryView.rows[0].value);
+  const db = await nano.use("uniqueuser");
+  // const newView = await new PostView();
+  // const insertDesign = await db.insert(newView, "_design/posts");
+  const tryView = await db.view("posts", "ups");
+  console.log(tryView);
+  // console.log(tryView.rows[0].value);
   // const view = await db.view("body_view", "body", { key: "sinus" });
   // const useView = db.view();
   await addDesignDoc("uniqueuser");
